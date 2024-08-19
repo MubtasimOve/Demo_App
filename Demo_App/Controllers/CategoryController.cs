0a73c8b7136e4dcd9de9d5c9f08468ec01575af6
Mubtasim Ihtisham Ove <mubtasim26@gmail.com>
2024-08-19 16:24:23 +0600

    Add README.md.

---
+++ Demo_App/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Demo_App.Data;
+using Demo_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List <Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Demo_App/Migrations/20240819065752_Update1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Method");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 3,
+                column: "Name",
+                value: "History");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Migration");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Category_Id", "DisplayOrder", "Name" },
+                values: new object[] { 5, 5, "Update" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Action");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Action");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Category_Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Action");
+        }
+    }
+}
