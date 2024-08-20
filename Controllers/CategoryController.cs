cac1469e0b0fab913b33d8418d0ba56be71314f5
Mubtasim Ihtisham Ove <mubtasim26@gmail.com>
2024-08-20 09:23:38 +0600

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Demo_App.Data;
+using Demo_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace Demo_App.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Demo_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_App.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Category_Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Category_Id = 2, Name = "Method", DisplayOrder = 2 },
+                new Category { Category_Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Category_Id = 4, Name = "Migration", DisplayOrder = 4 },
+                new Category { Category_Id = 5, Name = "Update", DisplayOrder = 5 }
+
+
+                );
+        }
+
+    }
+}
